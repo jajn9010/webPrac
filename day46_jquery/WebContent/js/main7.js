38ac5b9ee0e83aeb6784aa45a9b38ecc6cd4e18c
Jaejin Lee <jaejin1112@gmail.com>
2023-06-02 12:21:46 +0900

    study JQuery in KOSA  at morning 23/06/02

--- day46_jquery/WebContent/js/main7.js
+++ day46_jquery/WebContent/js/main7.js
@@ -1,10 +1,53 @@
 /* main7.js */
 
+const person = {
+	firstName: 'jaejin'
+	,lastName: 'Lee'
+	,getFullName: function() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+	,setFirstName: function(data) {
+		this.firstName = data;
+	}
+}
+
+console.log(person);
+console.log(person.lastName);
+person.setFirstName('Yuna');
+console.log(person.getFullName());
+
+const any = {
+	firstName: 'Any'
+	,lastName: 'Clarke'
+	,getFullName : function() {
+		return `${this.firstName} ${this.lastName}`
+	}
+	,setFirstName: function(data) {
+		this.firstName = data;
+	}
+}
+
+console.log(any);
+console.log(any.firstName);
+any.setFirstName('Lee');
+console.log(any.getFullName());
+
+function User(first, last) {
+	this.firstName = first;
+	this.lastName = last;
+	this.getFullName = function(){
+		return `${this.firstName} ${this.lastName}`
+	}
+}
+
+const saram = User('person', 'any');
+console.log(saram)
+
 //this
 //일반(Normal) 함수는 호출 위치에서 this 정의
 //화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의
 
-function User(name) {
+function User2(name) {
 	this.name = name;
 }
 
